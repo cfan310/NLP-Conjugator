82f1f7f701c8400f3dcdefb2ea02887c5716bf80
naffer <cfanning43@gmail.com>
2024-03-13 10:01:34 -0400

    added conjugator function body

--- conjugator.py
+++ conjugator.py
@@ -4,5 +4,17 @@
 
 import spacy 
 
-nlpModel = spacy.load('es_core_web_lg')
+nlpModel = spacy.load('es_core_web_lg') # load model
+
+def conjugate(verb, tense):
+	# takes spanish infinitive verb as input
+	# also takes desired tense (present, preterite, future, etc.)
+	# returns the conjugated verb
+	doc = nlp(verb)	# parse verb using spacy
+	if doc[0].pos = "VERB":
+		return doc[0].conjugate(tense=tense)
+	else:
+		return "Not a verb" # handles non-verb input 
+
+user_verbInput = input("Enter a Spanish verb: ")
 
