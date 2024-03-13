0f3475f75e821ed67136b7aaf5e8a7ea51de185e
naffer <cfanning43@gmail.com>
2024-03-13 10:12:39 -0400

    prints input form asking for spanish verb, followed by an error

--- conjugator.py
+++ conjugator.py
@@ -4,17 +4,19 @@
 
 import spacy 
 
-nlpModel = spacy.load('es_core_web_lg') # load model
+nlpModel = spacy.load('es_core_news_lg') # load model
 
 def conjugate(verb, tense):
 	# takes spanish infinitive verb as input
 	# also takes desired tense (present, preterite, future, etc.)
 	# returns the conjugated verb
-	doc = nlp(verb)	# parse verb using spacy
-	if doc[0].pos = "VERB":
+	doc = nlpModel(verb)	# parse verb using spacy
+	if doc[0].pos == "VERB":
 		return doc[0].conjugate(tense=tense)
 	else:
 		return "Not a verb" # handles non-verb input 
 
 user_verbInput = input("Enter a Spanish verb: ")
+conjugated_verb = conjugate_verb(user_verb, "present") # can change verb tense as needed
+print(f"The verb '{user_verbInput}' conjugated in present tense is: {conjugated_verb}")
 
